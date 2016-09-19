e769c5fcbe9edd1b36cf12aeef049e6906397a04
shiaujib <ch23173885@yahoo.com.tw>
2016-09-19 17:31:56 +0800

    finish 7 sensors version

--- dataPlot.py
+++ dataPlot.py
@@ -12,10 +12,17 @@ def sensorplot():
     # loop to update the data 
     global result
     value=[3]*100
-    zero=[0]*1000
-    fig = plt.figure()
-    sen1f = fig.add_subplot(211)
-    sen2f = fig.add_subplot(212)
+    zero=[10]*1000
+    fig = plt.figure(figsize=(12,12))
+    sen1f = fig.add_subplot(711)
+    sen2f = fig.add_subplot(712)
+    sen3f = fig.add_subplot(713)
+    sen4f = fig.add_subplot(714)
+    sen5f = fig.add_subplot(715)
+    sen6f = fig.add_subplot(716)
+    sen7f = fig.add_subplot(717)
+    fig.tight_layout()
+    fig.title="Sensor Data"
     # some X and Y data
     x = np.arange(1000)
     y = zero[0:1000]
@@ -23,14 +30,45 @@ def sensorplot():
     sen1f.set_xlabel('time')
     sen1f.set_ylim((0,20))
     sen1f.set_xlim((0,1000))
+    sen1fr, = sen1f.plot(x, y)
+    ###########################################
     sx = np.arange(10000)
-    sy = np.random.randn(10000)
+    sy = zero[0:1000]
     sen2f.set_ylabel('value')
     sen2f.set_xlabel('time')
     sen2f.set_ylim((0,20))
     sen2f.set_xlim((0,1000))
-    sen1fr, = sen1f.plot(x, y)
-    sen2fr, = sen2f.plot(sx,sy)
+    sen2fr, = sen2f.plot(x,y)
+    ###########################################
+    sen3f.set_ylabel('value')
+    sen3f.set_xlabel('time')
+    sen3f.set_ylim((0,20))
+    sen3f.set_xlim((0,1000))
+    sen3fr, = sen3f.plot(x,y)
+    ###########################################
+    sen4f.set_ylabel('value')
+    sen4f.set_xlabel('time')
+    sen4f.set_ylim((0,20))
+    sen4f.set_xlim((0,1000))
+    sen4fr, = sen4f.plot(x,y)
+    ###########################################
+    sen5f.set_ylabel('value')
+    sen5f.set_xlabel('time')
+    sen5f.set_ylim((0,20))
+    sen5f.set_xlim((0,1000))
+    sen5fr, = sen5f.plot(x,y)
+    ###########################################
+    sen6f.set_ylabel('value')
+    sen6f.set_xlabel('time')
+    sen6f.set_ylim((0,20))
+    sen6f.set_xlim((0,1000))
+    sen6fr, = sen6f.plot(x,y)
+    ###########################################
+    sen7f.set_ylabel('value')
+    sen7f.set_xlabel('time')
+    sen7f.set_ylim((0,20))
+    sen7f.set_xlim((0,1000))
+    sen7fr, = sen7f.plot(x,y)
     # draw and show it
     fig.canvas.draw()
     plt.show(block=False)
@@ -44,6 +82,12 @@ def sensorplot():
             y[-99:] = result[0:99]
             # set the new data
             sen1fr.set_ydata(y)
+            sen2fr.set_ydata(y)
+            sen3fr.set_ydata(y)
+            sen4fr.set_ydata(y)
+            sen5fr.set_ydata(y)
+            sen6fr.set_ydata(y)
+            sen7fr.set_ydata(y)
             fig.canvas.draw()
         except KeyboardInterrupt:
             break
